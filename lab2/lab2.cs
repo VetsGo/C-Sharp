79a831837dc1ee3e71a53a18a0915050e4c7b332
Ridcovets_Sergo <55623322+VetsGo@users.noreply.github.com>
2022-09-19 23:42:59 +0300

    lab2 task 15 (30)

---
+++ lab2/lab2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter nn = ");
+            int nn = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter nk = ");
+            int nk = Convert.ToInt32(Console.ReadLine());
+            double p = 1;
+
+            if (0 <= nn && nn <= nk)
+            {
+                for (int i = nn; i <= nk; i++)
+                {
+                    p *= (Math.Pow(i, 2) + Math.Pow(-1, Math.Pow(i, 2) + Math.Pow(-1, i) * i) * i) / (3 * Math.Pow(i, 2) + 5);
+                }
+                Console.WriteLine("The product of a number series = " + p);
+            }
+            else
+            {
+                Console.WriteLine("<0 <= nn <= nk> condition is not fulfilled!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
