e1b2ea950af23e17516383cee334d3b8b20fd7c4
Ridcovets_Sergo <55623322+VetsGo@users.noreply.github.com>
2022-09-19 23:23:41 +0300

    lab1 option 15 (30)
    Finished lab 1 task 15 homework

---
+++ ConsoleApp1/lab1/lab1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace lab1
+{
+    class lab1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter x = ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double num = 0;
+
+            num = 2 * Math.Pow(x, 4) - 3 * Math.Pow(x, 3) + 4 * Math.Pow(x, 2) - 5 * x + 6;
+            Console.WriteLine("num = " + num);
+            Console.ReadLine();
+        }
+    }
+}
